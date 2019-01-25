588bda4885d3223c5711784181130bca7aeb79ed
vladplesu <vladp1987@gmail.com>
2019-01-25 07:53:23 +0200

    added hover effect for social media + refactored app.js

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,8 @@ const $searchResultsSmall = $('#search-sm'),
       $productCards       = $('.card'),
       $blogNews           = $('[id^="blog-news"]'),
       $twitterPosts       = $('[id^="post"]'),
-      $flickers           = $('#flicker-widget').children('div').children('a');;
+      $flickers           = $('#flicker-widget').children('div').children('a'),
+      $socialMedia        = $('#social-media').children('a');
 
 // Add hover effect for cart and wishlist buttons
 $('.navbar a').hover(
@@ -20,25 +21,13 @@ $('.navbar a').hover(
 );
 
 // Add hover effect for carousel action buttons
-$carouselActions.find('a').hover(
-    function() {
-        console.log("test");
-        $(this).removeClass('text-secondary').addClass('text-info');
-    },
-    function() {
-        $(this).removeClass('text-info').addClass('text-secondary');
-    }
-);
+setHover($carouselActions.find('a'));
 
 // Add hover effect for product action buttons
-$productActions.find('a').hover(
-    function() {
-        $(this).removeClass('text-secondary').addClass('text-info');
-    },
-    function() {
-        $(this).removeClass('text-info').addClass('text-secondary');
-    }
-);
+setHover($productActions.find('a'));
+
+// Add hover effect for social media buttons
+setHover($socialMedia);
 
 // Add sliding effect for product buttons on card hover
 $productActions.hide();
@@ -54,40 +43,10 @@ $productCards.hover(
 )
 
 // get search result from json file on keyup event for medium and large devices
-$searchFieldLarge.keyup(function () {
-    $searchResultsLarge.html('').show();
-    let searchVal = $searchFieldLarge.val();
-    let expression = new RegExp(searchVal, 'i');
-    $.getJSON('../../public/data.json', function (data) {
-        $.each(data, function (key, value) {
-            if (value.result.search(expression) !== -1) {
-                $searchResultsLarge.append('<a href="#" class="list-group-item list-group-item-action border-0 bg-secondary text-light rounded-0">' + value.result + '</a>');
-                $searchResultsLarge.find('a').click(function (e) {
-                    $searchFieldLarge.val(e.target.textContent);
-                    $searchResultsLarge.slideUp('slow');
-                });
-            }
-        });
-    });
-});
+setSearchItems($searchFieldLarge, $searchResultsLarge,  ' bg-secondary text-light rounded-0')
 
 // get search result from json file on keyup event for small devices
-$searchFieldSmall.keyup(function () {
-    $searchResultsSmall.html('').show();
-    let searchVal = $searchFieldSmall.val();
-    let expression = new RegExp(searchVal, 'i');
-    $.getJSON('../../public/data.json', function (data) {
-        $.each(data, function (key, value) {
-            if (value.result.search(expression) !== -1) {
-                $searchResultsSmall.append('<a href="#" class="list-group-item list-group-item-action border-0">' + value.result + '</a>');
-                $searchResultsSmall.find('a').click(function (e) {
-                    $searchFieldSmall.val(e.target.textContent);
-                    $searchResultsSmall.slideUp('slow');
-                });
-            }
-        });
-    });
-});
+setSearchItems($searchFieldSmall, $searchResultsSmall)
 
 // update blog news
 updateNews();
@@ -97,26 +56,18 @@ function updateNews() {
     $.getJSON('../../public/news.json', function(data) {
         let first = getRandom(5);
         let second = first < data.length - 1 ? first + 1 : 0;
-        $($blogNews[0]).html('').append(
-            '<div class="bg-info text-white py-2 px-3 text-center">\
-                <p class="h11 font-weight-bold m-0 text-uppercase">' + data[first].month + '</p>\
-                <p class="h9 font-weight-bold m-0">' + data[first].day + '</p>\
-            </div>\
-            <div class="col">\
-                <h4 class="h8 font-weight-bold mb-0">' + data[first].newsTitle + '</h4>\
-                <p class="h10">' + data[first].content + '</p>\
-            </div>'
-        );
-        $($blogNews[1]).html('').append(
-            '<div class="bg-info text-white py-2 px-3 text-center">\
-                <p class="h11 font-weight-bold m-0 text-uppercase">' + data[second].month + '</p>\
-                <p class="h9 font-weight-bold m-0">' + data[second].day + '</p>\
-            </div>\
-            <div class="col">\
-                <h4 class="h8 font-weight-bold mb-0">' + data[second].newsTitle + '</h4>\
-                <p class="h10">' + data[second].content + '</p>\
-            </div>'
-        );
+        let arr = [first, second];
+        $.each($blogNews, function(i, val) {
+            $(val).html('').append(
+                '<div class="bg-info text-white py-2 px-3 text-center">\
+                    <p class="h11 font-weight-bold m-0 text-uppercase">' + data[arr[i]].month + '</p>\
+                    <p class="h9 font-weight-bold m-0">' + data[arr[i]].day + '</p>\
+                </div>\
+                <div class="col">\
+                    <h4 class="h8 font-weight-bold mb-0">' + data[arr[i]].newsTitle + '</h4>\
+                    <p class="h10">' + data[arr[i]].content + '</p>\
+                </div>');
+            });
     });
 }
 
@@ -128,18 +79,15 @@ function updatePosts() {
     $.getJSON('../../public/posts.json', function(data) {
         let first = getRandom(5);
         let second = first < data.length - 1 ? first + 1 : 0;
-        $($twitterPosts[0]).html('').append(
-            '<p class="mb-0">\
-                <a href="#" class="text-info">@' + data[first].user + ' </a>' + data[first].post + '<a href="#" class="text-info">' + (data[first].shoutouts.length === 0 ? '' : ' @' + data[first].shoutouts) + '</a>\
-            </p>\
-            <span><small><em>' + getDays(data[first].date) + ' days ago</em></small></span>'
-        );
-        $($twitterPosts[1]).html('').append(
-            '<p class="mb-0">\
-                <a href="#" class="text-info">@' + data[second].user + ' </a>' + data[second].post + '<a href="#" class="text-info">' + (data[second].shoutouts.length === 0 ? '' : ' @' + data[second].shoutouts) + '</a>\
-            </p>\
-            <span><small><em>' + getDays(data[second].date) + ' days ago</em></small></span>'
-        );
+        let arr = [first, second];
+        $.each($twitterPosts, function(i, val) {
+            $(val).html('').append(
+                '<p class="mb-0">\
+                    <a href="#" class="text-info">@' + data[arr[i]].user + ' </a>' + data[arr[i]].post + '<a href="#" class="text-info">' + (data[arr[i]].shoutouts.length === 0 ? '' : ' @' + data[arr[i]].shoutouts) + '</a>\
+                </p>\
+                <span><small><em>' + getDays(data[arr[i]].date) + ' days ago</em></small></span>'
+            );
+        });
     });
 }
 
@@ -153,6 +101,7 @@ $flickers.on('click', function(e) {
     $('#flicker').modal('show');
 });
 
+// helper functions
 function getRandom(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
@@ -160,4 +109,34 @@ function getRandom(max) {
 function getDays(date) {
     let today = Date.now();
     return Math.round((today - Date.parse(date))/(1000*60*60*24));
+}
+
+function setHover($elem) {
+    $elem.hover(
+        function() {
+            $(this).removeClass('text-secondary').addClass('text-info');
+        },
+        function() {
+            $(this).removeClass('text-info').addClass('text-secondary');
+        }
+    );
+}
+
+function setSearchItems($searchField, $searchResults, itemsClass = '') {
+    $searchField.keyup(function () {
+        $searchResults.html('').show();
+        let searchVal = $searchField.val();
+        let expression = new RegExp(searchVal, 'i');
+        $.getJSON('../../public/data.json', function (data) {
+            $.each(data, function (key, value) {
+                if (value.result.search(expression) !== -1) {
+                    $searchResults.append('<a href="#" class="list-group-item list-group-item-action border-0' + itemsClass + '">' + value.result + '</a>');
+                    $searchResults.find('a').click(function (e) {
+                        $searchField.val(e.target.textContent);
+                        $searchResults.slideUp('slow');
+                    });
+                }
+            });
+        });
+    });
 }
