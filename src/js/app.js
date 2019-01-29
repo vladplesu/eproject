c3387c330626b6a7c6d240f37d19b82e18a81bc3
vladplesu <vladp1987@gmail.com>
2019-01-29 05:17:00 +0200

    modified some css properties for product cards for better design

--- src/js/app.js
+++ src/js/app.js
@@ -12,26 +12,17 @@ const $searchResultsSmall = $('#search-sm'),
       $listSwitch         = $('#list'),
       $gridSwitch          = $('#grid');
 
+// Default layout depending on screen width
+if ($(window).width() < 768) {
+    switchLayout();
+    $productCards.off('mouseenter mouseleave');
+    $productActions.css('display', '');
+}
+
 // Switch products layout to a list layout
 $listSwitch.on('click', function () {
     if (!$productCards.hasClass('card-custom')) {
-        let cardClasses = ['card-custom', 'col-md', 'mr-md-3'];
-        let cardBodyClasses = [
-            'card-body-custom',
-            'pl-3',
-            'border-top',
-            'mx-0',
-            'pr-0',
-            'py-0',
-            'd-flex',
-            'flex-column',
-            'justify-content-center'];
-        let spanClasses = ['p-1', 'rounded', 'py-4', 'px-2', 'position-absolute'];
-        $('#products').children('div').not(':first').toggleClass('flex-column');
-        $productCards.toggleClass(cardClasses);
-        $productCards.children('img').toggleClass('card-img-top');
-        $productCards.children('div').toggleClass(cardBodyClasses);
-        $productCards.children('div').children('span').toggleClass(spanClasses);
+        switchLayout();
         $productCards.off('mouseenter mouseleave');
         $productActions.css('display', '');
     }
@@ -40,23 +31,7 @@ $listSwitch.on('click', function () {
 // Switch product layout to a grid layout
 $gridSwitch.on('click', function() {
     if ($productCards.hasClass('card-custom')) {
-        let cardClasses = ['card-custom', 'col-md', 'mr-md-3'];
-        let cardBodyClasses = [
-            'card-body-custom',
-            'pl-3',
-            'border-top',
-            'mx-0',
-            'pr-0',
-            'py-0',
-            'd-flex',
-            'flex-column',
-            'justify-content-center'];
-        let spanClasses = ['p-1', 'rounded', 'py-4', 'px-2', 'position-absolute'];
-        $('#products').children('div').not(':first').toggleClass('flex-column');
-        $productCards.toggleClass(cardClasses);
-        $productCards.children('img').toggleClass('card-img-top');
-        $productCards.children('div').toggleClass(cardBodyClasses);
-        $productCards.children('div').children('span').toggleClass(spanClasses);
+        switchLayout()
         showActionBtns();
     }
 })
@@ -69,7 +44,7 @@ $('.navbar a').hover(
     function() {
         $(this).find('span').toggleClass(['badge-info text-white', 'badge-secondary']);
     }
-);
+    );
 
 // Add hover effect for carousel action buttons
 setHover($carouselActions.find('a'));
@@ -86,13 +61,13 @@ function showActionBtns() {
     if ($(window).width() >= 768) {
         $productActions.hide();
         $productCards
-            .on('mouseenter', function () {
+        .on('mouseenter', function () {
                 $(this).find('[id^="product-actions"]').show('slow');})
-            .on('mouseleave', function() {
-                $(this).find('[id^="product-actions"]').slideUp('slow');
+                .on('mouseleave', function() {
+                    $(this).find('[id^="product-actions"]').slideUp('slow');
             });
+        }
     }
-}
 
 // get search result from json file on keyup event for medium and large devices
 setSearchItems($searchFieldLarge, $searchResultsLarge,  ' bg-secondary text-light rounded-0')
@@ -112,9 +87,9 @@ function updateNews() {
         $.each($blogNews, function(i, val) {
             $(val).html('').append(
                 '<div class="bg-info text-white py-2 px-3 text-center">\
-                    <p class="h11 font-weight-bold m-0 text-uppercase">' + data[arr[i]].month + '</p>\
+                <p class="h11 font-weight-bold m-0 text-uppercase">' + data[arr[i]].month + '</p>\
                     <p class="h9 font-weight-bold m-0">' + data[arr[i]].day + '</p>\
-                </div>\
+                    </div>\
                 <div class="col">\
                     <h4 class="h8 font-weight-bold mb-0">' + data[arr[i]].newsTitle + '</h4>\
                     <p class="h10">' + data[arr[i]].content + '</p>\
@@ -136,8 +111,8 @@ function updatePosts() {
             $(val).html('').append(
                 '<p class="mb-0">\
                     <a href="#" class="text-info">@' + data[arr[i]].user + ' </a>' + data[arr[i]].post + '<a href="#" class="text-info">' + (data[arr[i]].shoutouts.length === 0 ? '' : ' @' + data[arr[i]].shoutouts) + '</a>\
-                </p>\
-                <span><small><em>' + getDays(data[arr[i]].date) + ' days ago</em></small></span>'
+                    </p>\
+                    <span><small><em>' + getDays(data[arr[i]].date) + ' days ago</em></small></span>'
             );
         });
     });
@@ -173,7 +148,7 @@ function setHover($elem) {
             //$(this).removeClass('text-info').addClass('text-secondary');
             $(this).toggleClass(['text-secondary', 'text-info']);
         }
-    );
+        );
 }
 
 function setSearchItems($searchField, $searchResults, itemsClass = '') {
@@ -193,4 +168,25 @@ function setSearchItems($searchField, $searchResults, itemsClass = '') {
             });
         });
     });
+}
+
+function switchLayout() {
+    let cardClasses = ['card-custom', 'col-md', 'mr-md-3'];
+    let cardBodyClasses = [
+        'card-body-custom',
+        'pl-3',
+        'border-top',
+        'mx-0',
+        'py-0',
+        'd-flex',
+        'flex-column',
+        'justify-content-center'];
+    let spanClasses = ['p-1', 'rounded', 'px-2', 'position-absolute', 'nowrap'];
+    $('#products').children('div').not(':first').toggleClass('flex-column');
+    $productCards.toggleClass(cardClasses);
+    $productCards.children('img').toggleClass('card-img-top');
+    $productCards.children('div').toggleClass(cardBodyClasses);
+    $productCards.children('div').children('span').not('#out-of-stock').toggleClass([...spanClasses, 'py-4']);
+    $('#out-of-stock').toggleClass([...spanClasses, 'py-3']);
+    $productCards.children('div').children('p').toggleClass('d-md-block');
 }
