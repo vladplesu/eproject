d224b29dc0aaafa60a6a019cec59e05d13a7a3c6
vladplesu <vladp1987@gmail.com>
2019-01-29 17:30:43 +0200

    added validator, updated register route to add emails

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let fs = require('fs');
 
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 let app = express();
 
 app.use(express.static('public'));
@@ -9,26 +12,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 let router = express.Router();
 
-router.post('/register', function (req, res) {
-    let user = { table: [{ email: req.body.useremail }] };
-    let obj = JSON.stringify(user);
+router.post('/register', [body('useremail').isEmail().normalizeEmail(), sanitizeBody('notifyOnReply').toBoolean()], function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    let user = { email: req.body.useremail };
     fs.readFile('public/subscribers.json', function (err, data) {
         if (err && err.code === 'ENOENT') {
-            return fs.writeFileSync('public/subscribers.json', obj, error => console.error);
+            let obj = [];
+            obj.push(user);
+            fs.writeFileSync('public/subscribers.json', JSON.stringify(obj), error => console.error);
         } else if (err) {
             console.error(err);
         } else {
             try {
                 const fileData = JSON.parse(data);
 
-                fileData.table.push(obj);
+                fileData.push(user);
 
-                return fs.writeFileSync('public/subscribers.json', fileData, error => console.error);
+                fs.writeFileSync('public/subscribers.json', JSON.stringify(fileData), error => console.error);
             } catch (exception) {
                 console.error(exception);
             }
         }
     });
+    res.send('success');
 });
 
 app.use(router);
